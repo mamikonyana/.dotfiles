62cd7ef074cc7e13a4c1c7c0196acdd441128b3a
Arsen Mamikonyan <arsen@locu.com>
2013-04-17 18:10:35 +0000

    Remove colors.config from repo, have colors.template instead

--- setup.sh
+++ setup.sh
@@ -6,3 +6,5 @@ sed -i "s+___BASE_DIRECTORY___+`pwd`+g" ~/.zshrc
 for script in ./small_scripts/*sh; do
   echo "alias $(grep '#short-name: ' $script | sed 's+#short-name: ++')='$(pwd)/$script'" >> ~/.zshrc
 done
+
+cp ./colors.template ./colors.config
