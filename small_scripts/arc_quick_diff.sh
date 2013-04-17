4d66aee6c81b47324b33dbb697c6ea9a6b9f36e7
Arsen Mamikonyan <arsen@locu.com>
2013-04-17 19:01:28 +0000

    Move quick_diff.sh to arc_quick_diff.sh

--- small_scripts/arc_quick_diff.sh
+++ small_scripts/arc_quick_diff.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 #short-name: quickdiff
 
-git checkout -b "$(echo $1 | sed 's/[^a-zA-Z]/_/g' | cut -c -20)-$(date '+%y%m%dT%H-%M-%S')"
+git checkout -b "$(echo $1 | sed 's/[^0-9a-zA-Z]/_/g' | cut -c -20)-$(date '+%y%m%dT%H-%M-%S')"
 git commit -a -m "$1"
 arc diff --verbatim --allow-untracked --reviewers $2
