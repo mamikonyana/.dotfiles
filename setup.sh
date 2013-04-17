3d64e887f8b35f273a2af892abc82a358908108c
Arsen Mamikonyan <arsen@locu.com>
2013-04-17 19:47:42 +0000

    Add --update flag to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,22 @@
 #!/bin/zsh
+
+INITIALIZE_CONFIG_FILES='non-empty-string'
+
+USAGE="
+Usage: `basename $0` [--update]
+"
+while [ "$1" != "" ]; do
+    case "$1" in
+        --update )
+            INITIALIZE_CONFIG_FILES=
+            ;;
+        * )
+            echo $USAGE
+            exit 1
+    esac
+    shift
+done
+
 cp .zshrc_template ~/.zshrc
 # TODO(arsen): only works on Linux
 sed -i "s+___BASE_DIRECTORY___+`pwd`+g" ~/.zshrc
@@ -7,4 +25,7 @@ for script in ./small_scripts/*sh; do
   echo "alias $(grep '#short-name: ' $script | sed 's+#short-name: ++')='$(pwd)/$script'" >> ~/.zshrc
 done
 
-cp ./colors.template ./colors.config
+if [ "$INITIALIZE_CONFIG_FILES" ] ; then
+  echo 'Copying config files..'
+  cp ./colors.template ./colors.config
+fi
