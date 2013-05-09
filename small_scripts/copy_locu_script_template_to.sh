6f9a11dbbbafcb4742f4b93ac4da08fcde942bbf
Arsen Mamikonyan <arsen@locu.com>
2013-05-09 22:23:02 +0000

    Update locu createscript function

--- small_scripts/copy_locu_script_template_to.sh
+++ small_scripts/copy_locu_script_template_to.sh
@@ -4,7 +4,9 @@
 cp "`git rev-parse --show-toplevel`/website/scripts/TEMPLATE_SCRIPT.py" $1
 
 # Rename the placeholders
-sed -i "s+___SCRIPT_NAME___+$1+g" $1
+sed -i "s+___SCRIPT_PATH___+$1+g" $1
+sed -i "s+___USER___+`whoami`+g" $1
+echo `whoami`
 
 # Start editing
 $EDITOR $1
