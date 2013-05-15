f7cc462527bd3bd98c6c4bc527dc3df3d144aa71
Arsen Mamikonyan <arsen@ip-10-172-239-136.us-west-1.compute.internal>
2013-05-15 01:45:24 +0000

    More vim configs

--- setup-vim.sh
+++ setup-vim.sh
@@ -1,17 +1,57 @@
 mkdir -p ~/.vim/autoload ~/.vim/bundle; \
-curl -so ~/.vim/autoload/pathogen.vim \
+csdfurl -so ~/.vim/autoload/pathogen.vim \
     https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
 
 cd ~/.vim/bundle
 git clone https://github.com/scrooloose/syntastic.git
 
-
-echo "call pathogen#infect()" > ~/.vim/vimrc
+sudo pip install flake8
 
 rm ~/.vimrc
 ln -s ~/.vim/vimrc ~/.vimrc
 
-cat >> ~/.vim/vimrc <<'_EOF'
+cat > ~/.vim/vimrc <<'_EOF'
+" Use pathogen to easily modify the runtime path to include all
+" plugins under the ~/.vim/bundle directory
+call pathogen#helptags()
+call pathogen#incubate()
+
 colorscheme delek
+syntax enable
+
+set autoindent
+
+" Get rid of menu and tool bars
+set guioptions-=m
+set guioptions-=T
+
+let g:syntastic_enable_signs=0
+let g:syntastic_python_checkers=['flake8', 'pyflakes']
+
+" Enable Mouse usage
+set mouse=a
+
+" Highlight search results
+set hlsearch
+
+set autowrite
+
+nnoremap ; :
+
+" Automatically remove trailing whitespaces
+autocmd BufWritePost *.py:%s/\s\+$//e
+
+" Temporary untill get used to key navigation
+map <up> <nop>
+map <down> <nop>
+map <left> <nop>
+map <right> <nop>
+
+" Clear highlighted search by ,/
+nmap <silent> ,/ :nohlsearch<CR>
+
+" Save if need root access to do so (even after opening as normal user)
+cmap w!! w !sudo tee % >/dev/null
+
 _EOF
 
