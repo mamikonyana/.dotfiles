988e49fa13edab1c5d32988650a1fe5706ad7970
Arsen Mamikonyan <arsen@mamikonyan.am>
2014-10-20 11:49:07 -0700

    Fix default vim install bug

--- setup.zsh
+++ setup.zsh
@@ -6,6 +6,7 @@ Usage: `basename $0` [--update]
 install_xmonad=false
 install_zsh=false
 force_install=false
+install_vim=false
 
 while [ "$1" != "" ]; do
     case "$1"  in
@@ -52,21 +53,24 @@ function setup_zsh
     else
         # TODO(arsen): Check that it point to a correct repository.
         if $force_install ; then
+            echo "Deleting the contents of ~/.oh-my-zsh"
             rm -rf ~/.oh-my-zsh
             git clone git@github.com:mamikonyana/oh-my-zsh.git ~/.oh-my-zsh
-            echo "Deleting the contents of ~/.oh-my-zsh"
         fi
     fi
+    echo "Copied default zsh config into ~/.zshrc"
     cp ./defaults/zshrc.zsh-template ~/.zshrc
     # Install esqaw theme
     # TODO(arsen): Figure out a way to get this variable from shell, it's there in terminal
     ZSH_CUSTOM=~/.oh-my-zsh/custom
     mkdir -p $ZSH_CUSTOM/themes
     curl https://raw.githubusercontent.com/mamikonyana/esqaw-zsh-theme/master/esqaw.zsh-theme -o $ZSH_CUSTOM/themes/esqaw.zsh-theme
+    echo "Done Setting up zsh."
 }
 
 function setup_vim
 {
+    echo "Installing vim using setup-vim.sh script"
    ./setup-vim.sh 
 }
 
